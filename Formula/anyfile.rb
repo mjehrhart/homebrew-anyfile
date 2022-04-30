d20933fa20163578fe0d79292da34f02a343f421
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-30 14:25:57 -0700

    v1.0.1

--- Formula/anyfile.rb
+++ Formula/anyfile.rb
@@ -1,9 +1,9 @@
  class Anyfile < Formula
   desc "Store and retrieve asset files from your public repo."
   homepage "https://github.com/mjehrhart/anyfile"
-  url "https://github.com/mjehrhart/anyfile/releases/download/v1.0.0/anyfile.tar.gz"
-  version "1.0.0"
-  sha256 "301bc903df915e9169f4b8b62cef68395097184f3bae28ef445d9cc2ecb47825"
+  url "https://github.com/mjehrhart/anyfile/releases/download/v1.0.1/anyfile.tar.gz"
+  version "1.0.1"
+  sha256 "8ef41ae5431173a4428d626fc69b4b66e39cfbe10e1a0971c1f733ed9015cf33"
   license "BSD 2-Clause License"
 
   def install
