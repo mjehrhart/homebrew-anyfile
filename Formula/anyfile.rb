016a0fead0f050295dc342f83225f95a6aad9fc9
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-30 20:42:29 -0700

    updated sha

--- Formula/anyfile.rb
+++ Formula/anyfile.rb
@@ -3,7 +3,7 @@
   homepage "https://github.com/mjehrhart/anyfile"
   url "https://github.com/mjehrhart/anyfile/releases/download/v1.0.4/anyfile.tar.gz"
   version "1.0.4"
-  sha256 "9bd59b5c598655dcfebf0047c87a682649d6291be22e2aad459a166edb9420e9"
+  sha256 "e046e5e0d9626744c44ad64a536ea656c435b849bec74ef7865622b3919556ba"
   license "BSD 2-Clause License"
 
   def install
